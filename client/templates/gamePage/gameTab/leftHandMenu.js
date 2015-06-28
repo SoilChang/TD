2f982e7c2fbd891c30897a72e706ce08d5613250
Dylan Chew <dylanchewzj@hotmail.com>
2015-06-28 19:53:56 +0800

    28 Jun 750pm
    update

--- client/templates/gamePage/gameTab/leftHandMenu.js
+++ client/templates/gamePage/gameTab/leftHandMenu.js
@@ -1,4 +1,8 @@
-
+Template.leftHandMenu.event({
+	'click #pauseBtn': function() {
+		togglePause();
+	}
+});
 /*left hand menu bar animation*/
 Template.leftHandMenu.onRendered(function(){
 	$('#c-game-left_hand_menu').css({'left':'180px', 'height':'540px'});
