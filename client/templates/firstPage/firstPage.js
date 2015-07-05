3ea79360dccfecc8e61cc6b6174807857b6953c8
Chang Ho Huan <im741314@gmail.com>
2015-07-05 12:59:15 +0800

    firstPage animation rework. Add like button to first page

--- client/templates/firstPage/firstPage.js
+++ client/templates/firstPage/firstPage.js
@@ -1,56 +1,63 @@
-Template.firstPage.helpers({
-
-});
-
-
-Template.firstPage.events({
+Template.firstPage.onRendered(function(){
 	// hide everything
-	'load #secTower': function(){
-		$('#photoBook, #credit,#enter').hide();
-	},
-	// hide and show photobook
-	'mouseleave #secTower': function(){
-		$('#secTower').height('100px');
-		$('#secTower').css('margin-left','452px');
-		$('#secTower').css('margin-top','169px');
-		$('#photoBook').hide();
-	},
+	$('#photoBook, #credit,#enter').hide();
 
-	'mouseenter #secTower':function(){
-		$('#secTower').height('130px');
-		$('#secTower').css('margin-left','440px');
-		$('#secTower').css('margin-top','145px');
-		$('#photoBook').show();
-	},
-	// hide and show credit banner
-	'mouseleave #lighthouse': function(){
-		$('#lighthouse').height('220px');
-		$('#lighthouse').css('margin-left','703px');
-		$('#lighthouse').css('margin-top','10px');
-		$('#credit').hide();
-	},
+	// animation for the second tower
+	$("#secTower").css({"margin-top":"-100px"});
+	$("#secTower").animate({marginTop:"169px"},2000, function(){
+		$("#secTower").hover(
+			function(){
+				$('#secTower').height('130px');
+				$('#secTower').css('margin-left','440px');
+				$('#secTower').css('margin-top','145px');
+				$('#photoBook').show();
+			},function(){
+				$('#secTower').height('100px');
+				$('#secTower').css('margin-left','452px');
+				$('#secTower').css('margin-top','169px');
+				$('#photoBook').hide();
+			});
+	});
 
-	'mouseenter #lighthouse':function(){
-		$('#lighthouse').height('250px');
-		$('#lighthouse').css('margin-left','695px');
-		$('#lighthouse').css('margin-top','-15px');
-		$('#credit').show();
-	},
+	// animation for the lighthouse
+	$("#lighthouse").css({"margin-top":"-200px"});
+	$("#lighthouse").animate({marginTop:"10px"},2000, function(){
+		$("#lighthouse").hover(
+			function(){
+				$('#lighthouse').height('250px');
+				$('#lighthouse').css('margin-left','695px');
+				$('#lighthouse').css('margin-top','-15px');
+				$('#credit').show();
+			},function(){
+				$('#lighthouse').height('220px');
+				$('#lighthouse').css('margin-left','703px');
+				$('#lighthouse').css('margin-top','10px');
+				$('#credit').hide();
+			});
+	});
 
-	// hide and show enter sign
-	'mouseleave #knight': function(){
-		$('#knight').height('100px');
-			$('#knight').css('margin-left','475px');
-			$('#knight').css('margin-top','415px');
-		$('#enter').hide();
-	},
+	// animation for the knight
+	$("#knight").css({"margin-left":"450px","margin-top":"450px", "height":"0px"});
+	$("#knight").animate({marginTop:"415px", marginLeft:"475px", height:"100px"},2000, function(){
+		$("#knight").hover(
+			function(){
+				$('#knight').height('120px');
+				$('#knight').css('margin-left','470px');
+				$('#knight').css('margin-top','395px');
+				$('#enter').show();
+			},function(){
+				$('#knight').height('100px');
+				$('#knight').css('margin-left','475px');
+				$('#knight').css('margin-top','415px');
+				$('#enter').hide();
+			});
+	});
 
-	'mouseenter #knight':function(){
-		$('#knight').height('120px');
-			$('#knight').css('margin-left','470px');
-			$('#knight').css('margin-top','395px');
-		$('#enter').show();
-	}
-
+	// facebook sdk stuff
+	 try {
+        FB.XFBML.parse();
+    }catch(e) {}  
 
 });
+
+
