9fb5f20b9fd3ffe5a3e7a19986a18a011fa1ec48
Chang Ho Huan <im741314@gmail.com>
2015-07-05 11:16:50 +0800

    silly like button from facebook working

--- client/helpers/facebook_SDK.js
+++ client/helpers/facebook_SDK.js
@@ -2,7 +2,8 @@ window.fbAsyncInit = function() {
     FB.init({
       appId      : '1605168309751569',
       status     : true,
-      xfbml      : true
+      xfbml      : true,
+      show_faces: false
     });
   };
 
