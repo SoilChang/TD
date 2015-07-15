ff50f77838da27047801a25c6f4ba0c7630943a1
Chang Ho Huan <im741314@gmail.com>
2015-07-15 23:58:58 +0800

    minor changes

--- client/main.js
+++ client/main.js
@@ -1,20 +1,25 @@
-loadSound();
+// loadSound();
 
-function loadSound() {
-    // Load the sound
-    createjs.Sound.alternateExtensions = ["mp3"];
-    createjs.Sound.on("fileload", handleFileLoad);
+// function loadSound() {
+//     // Load the sound
+//     createjs.Sound.alternateExtensions = ["mp3"];
+//     createjs.Sound.on("fileload", handleFileLoad);
 
-    createjs.Sound.registerSound("/sound/gameBG.ogg",'backgroundSound');
-    createjs.Sound.registerSound("/sound/laugh.ogg",'gameOverSound');
-    createjs.Sound.registerSound("/sound/ice.ogg",'iceSound');
-    createjs.Sound.registerSound("/sound/laser.ogg",'laserSound');
-}
+//     createjs.Sound.registerSound("/sound/gameBG.ogg",'backgroundSound');
+//     createjs.Sound.registerSound("/sound/laugh.ogg",'gameOverSound');
+//     createjs.Sound.registerSound("/sound/ice.ogg",'iceSound');
+//     createjs.Sound.registerSound("/sound/laser.ogg",'laserSound');
+// }
 
 
-function handleFileLoad(event) {
-    // Play the loaded sound
-    BGsound = createjs.Sound.play('backgroundSound',{loop:-1});
-    BGsound.volume = .1
+// function handleFileLoad(event) {
+//     // Play the loaded sound
+//     BGsound = createjs.Sound.play('backgroundSound',{loop:-1});
+//     BGsound.volume = .1
 
-}
+// }
+
+// buzz.defaults.autoplay = true;
+// buzz.defaults.loop = true;
+// var gameBG = new buzz.sound('/sound/gameBG.ogg');
+
