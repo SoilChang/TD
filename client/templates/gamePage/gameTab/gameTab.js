885ab66d2f0491b230d297b6e1095d66e260f43c
Dylan Chew <dylanchewzj@hotmail.com>
2015-07-16 23:17:58 +0800

    remove zakum gate

--- client/templates/gamePage/gameTab/gameTab.js
+++ client/templates/gamePage/gameTab/gameTab.js
@@ -42,7 +42,6 @@ coordinates, castleData, wave, //game stats
 s1, s2, s3, s4,//monster sprites
 backgroundI, background, castleIm, castleI, castle, //canvas images & variable
 castleLifebar,castleHp, castleHpI, castleText,
-gateI,gate,
 castleHitI, castleHit, monsterKillI, //added animations
 lightTower1, lightTower2, lightTower3, lightTower4,//tower images
 iceTower1, iceTower2, iceTower3, iceTower4,
@@ -469,13 +468,6 @@ function imageload() {
     backgroundI.src = "/images/gameImages/3dStage.png"
     //load background
     background = new createjs.Bitmap(backgroundI);
-    //gate
-    gateI = new Image();
-    gateI.src = "/images/gameImages/zakumGate.png"
-    //load background
-    gate = new createjs.Bitmap(gateI);
-    gate.x = 64
-    gate.y = -6
 
     //castle image
     castleIm = new Image();
@@ -666,7 +658,6 @@ function newGame() {
     castleHpI.sourceRect = new createjs.Rectangle(0,0,64,10);
 
     stage.addChild(castle)
-    stage.addChild(gate)
     //line of creep path
     //path();
 
@@ -729,7 +720,6 @@ function currentGame() {
 
     stage.enableMouseOver(0);
     stage.addChild(castle);
-    stage.addChild(gate)
     stage.addChild(pScreen);
 
     stage.update();
@@ -763,7 +753,6 @@ function continueGame() {
 
     stage.enableMouseOver(0);
     stage.addChild(castle)
-    stage.addChild(gate)
     stage.addChild(pScreen);
 
     stage.update();
@@ -1792,7 +1781,7 @@ function towerAttacks() {
                     continue;
                 };
                 for (var j=0;j<monsters.length;j++) {
-                    if (inRange(towers[i],monsters[j]) && monsters[j].y>=35) {
+                    if (inRange(towers[i],monsters[j]) && monsters[j].y>=0) {
                         cShots(towers[i],monsters[j])
                         towers[i].cd = towers[i].maxCd;
                         break;
@@ -2052,8 +2041,7 @@ function nextWave() {
         else {
             cMonster("mario",9);
             monsterData["mario"]["hp"]*=1.3
-        }
-        stage.addChild(gate)        
+        }       
     }
 }
 
